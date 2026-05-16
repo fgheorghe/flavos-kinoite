da27c6c50125bdadf7a863760c175bd81a6144fc
Grosan Flaviu Gheorghe <1872824+fgheorghe@users.noreply.github.com>
2026-05-16 21:19:08 +0100

    Create aliases.sh

---
+++ files/system/etc/profile.d/aliases.sh
@@ -0,0 +1,18 @@
+# files/system/etc/profile.d/aliases.sh
+
+# Modern CLI replacements
+alias ls='eza'
+alias ll='eza -la --git'
+alias lt='eza --tree --level=2'
+alias cat='bat --paging=never'
+alias find='fd'
+alias grep='rg'
+alias ps='procs'
+alias df='duf'
+alias du='dua'
+alias top='btop'
+alias sed='sd'
+alias help='tldr'
+
+# zoxide (smarter cd)
+eval "$(zoxide init bash)"
